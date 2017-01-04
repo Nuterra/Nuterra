b70335d84eaff031cb69e1e158b82ab38984a94e
mooshbeef <bramkamies@gmail.com>
2017-01-04 17:21:03 +0100

    Added sylver to credits list

--- src/Maritaria/SplashScreenHandler.cs
+++ src/Maritaria/SplashScreenHandler.cs
@@ -12,7 +12,7 @@ namespace Maritaria
 
 		public GUIStyle _style = new GUIStyle();
 
-		public string _contentFormat = "<color=#eeeeee><size=20>Mod by <color=#{0}>Maritaria</color></size></color>";
+		public string _contentFormat = "<color=#eeeeee><size=20>Mod by <color=#{0}>Maritaria</color> and <color={1}>Sylver</color></size></color>";
 
 		public SplashScreenHandler()
 		{
@@ -29,7 +29,9 @@ namespace Maritaria
 		{
 			if (CanDraw())
 			{
-				_content.text = string.Format(_contentFormat, GetFormattedColor(GetRainbowColor()));
+				string colorMaritaria = GetFormattedColor(GetRainbowColor(0f));
+				string colorSylver = GetFormattedColor(GetRainbowColor(1f));
+				_content.text = string.Format(_contentFormat, colorMaritaria, colorSylver);
 				Vector2 vector = new Vector2(Screen.width * 0.5f, Screen.height * 0.8f);
 				Vector2 vector2 = _style.CalcSize(_content);
 				vector.x -= vector2.x * 0.5f;
@@ -51,9 +53,9 @@ namespace Maritaria
 			return _splashScreen.CanvasTrans.GetComponent<Canvas>();
 		}
 
-		public static Color GetRainbowColor()
+		public static Color GetRainbowColor(float offset)
 		{
-			return Color.HSVToRGB(Mathf.Sin(Time.time) * 0.5f + 0.5f, 1f, 1f);
+			return Color.HSVToRGB(Mathf.Sin(Time.time - offset) * 0.5f + 0.5f, 1f, 1f);
 		}
 
 		public static string GetFormattedColor(Color color)
