5b560e7724e0d4f339b2779f8d99bbca0a80ff0c
mooshbeef <bramkamies@gmail.com>
2017-01-15 03:08:30 +0100

    Cleaned merger project

--- src/Nuterra.Installer/Merger.cs
+++ src/Nuterra.Installer/Merger.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using dnlib.DotNet;
 using dnlib.DotNet.Writer;
 using Nuterra.Installer.ModuleImport;
@@ -12,16 +8,6 @@ namespace Nuterra.Installer
 {
 	public static class Merger
 	{
-
-		public static void Main()
-		{
-			string managedDir = @"MergeTest";
-			string terraTechAssembly = Path.Combine(managedDir, "TerraTest.dll");
-			string nuterraAssembly = Path.Combine(managedDir, "NuterraTest.dll");
-			string outputFile = Path.Combine(managedDir, "MergedTest.dll");
-			MergeNuterra(managedDir, terraTechAssembly, nuterraAssembly, outputFile);
-		}
-
 		public static void MergeNuterra(string managedDir, string terraTechAssemblyFile, string nuterraAssemblyFile, string outputFile)
 		{
 			AssemblyResolver resolver = new AssemblyResolver();
@@ -38,9 +24,7 @@ namespace Nuterra.Installer
 			ModuleWriterOptions options = new ModuleWriterOptions();
 			options.MetaDataOptions.Flags = MetaDataFlags.PreserveRids;
 
-
 			terraTech.Write(outputFile, options);
-
 		}
 	}
 }
