404d646c346705545f70152d39f56f1e4858ca11
mooshbeef <bramkamies@gmail.com>
2017-01-17 00:20:27 +0100

    Added GalaxyCSharp dependency
    Installer now places the dependency in /Managed

--- src/Nuterra.Installer/FullInstallProgram.cs
+++ src/Nuterra.Installer/FullInstallProgram.cs
@@ -7,11 +7,12 @@ namespace Nuterra.Installer
 {
 	internal static class FullInstallProgram
 	{
-		public static readonly string NuterraDir = "Nuterra_Data";
+		public static readonly string NuterraDataDir = "Nuterra_Data";
 		public static readonly string NuterraAssemblyFile = "Nuterra.dll";
 		public static readonly string ExpectedHashFile = Path.Combine(NuterraDir, "installer.hash.txt");
 		public static readonly string AccessFile = Path.Combine(NuterraDir, "installer.access.txt");
 		public static readonly string TempOutputFile = Path.Combine(NuterraDir, "installer.modded.dll");
+		public static readonly string GalaxyAssemblyFile = "GalaxyCSharp.dll";
 
 		internal static void Main(string[] args)
 		{
@@ -64,6 +65,9 @@ namespace Nuterra.Installer
 
 			Console.WriteLine("Installing modded Assembly-CSharp.dll");
 			File.Copy(TempOutputFile, assemblyCSharpPath, overwrite: true);
+
+			//Install GalaxyCSharp.dll dependency
+			File.Copy(Path.Combine(NuterraDataDir, GalaxyAssemblyFile), Path.Combine(terraTechManagedDir, GalaxyAssemblyFile), overwrite: true);
 
 			Console.WriteLine("Install completed, have fun :3");
 			Console.ReadLine();
