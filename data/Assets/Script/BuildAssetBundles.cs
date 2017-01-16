f4d8160d7e77cf165564dbf274fe04d6926c8710
mooshbeef <bramkamies@gmail.com>
2017-01-16 23:28:57 +0100

    Added and loaded unity assets

---
+++ data/Assets/Script/BuildAssetBundles.cs
@@ -0,0 +1,14 @@
+﻿using System.IO;
+using UnityEditor;
+
+public class BuildAssetBundles
+{
+	public static readonly string AssetBundlesOutputFolder = "GeneratedAssetBundles";
+
+	[MenuItem("Nuterra/Build AssetBundles")]
+	private static void BuildAllAssetBundles()
+	{
+		Directory.CreateDirectory(AssetBundlesOutputFolder);
+		BuildPipeline.BuildAssetBundles(AssetBundlesOutputFolder, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+	}
+}
