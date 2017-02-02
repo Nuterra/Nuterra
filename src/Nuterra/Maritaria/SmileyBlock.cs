e5bfdc9be2b89792c14afafb4a8c4fbe4077e918
mooshbeef <bramkamies@gmail.com>
2017-02-02 02:53:55 +0100

    Cockpit block now uses prefab model

--- src/Nuterra/Maritaria/SmileyBlock.cs
+++ src/Nuterra/Maritaria/SmileyBlock.cs
@@ -8,11 +8,11 @@ namespace Maritaria
 	public sealed class SmileyBlock : CustomBlock
 	{
 		public static readonly string SpriteFile = @"Assets/Blocks/Cockpit/GSO_Observatory_icon.png";
-		public static readonly string ModelFile = @"Assets/Blocks/Cockpit/GSO_Observatory.blend";
+		public static readonly string ModelFile = @"Assets/Blocks/Cockpit/TT_GSO_Observatory_Block.blend";
 		public static readonly string MaterialFile = @"Assets/Blocks/Cockpit/GSO_Observatory_material.mat";
 
 		public static readonly int BlockID = 9000;
-
+		private static GameObject renderPrefab;
 		int CustomBlock.BlockID => BlockID;
 		public string Name => "GSO Cockpit";
 		public string Description => "Pop in here and have a first-person look at the world from this block";
@@ -39,22 +39,23 @@ namespace Maritaria
 			TankBlock tankBlock = Prefab.GetComponent<TankBlock>();
 
 			tankBlock.m_BlockCategory = Category;
-			tankBlock.attachPoints = new Vector3[]{//TODO: bottom only
-				new Vector3(0, 0, 0.5f), //Top
-				new Vector3(0, 0, -0.5f), //Bottom
-				//new Vector3(0, 0.5f, 0), //Front
-				new Vector3(0, -0.5f, 0), //Back
-				new Vector3(0.5f, 0, 0), //Left
-				new Vector3(-0.5f, 0, 0), //Right
+			tankBlock.attachPoints = new Vector3[]{
+				new Vector3(0, -0.5f, 0),
 			};
 			tankBlock.filledCells = new Vector3[] { new Vector3(0, 0, 0) };
 			tankBlock.partialCells = new Vector3[] { };
 
-			GameObject renderObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			//GameObject renderObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			GameObject renderObject = AssetBundleImport.Load<GameObject>(ModelFile);
+			renderPrefab = renderObject;
+			//GameObject.DontDestroyOnLoad(renderPrefab);
+			Console.WriteLine("Renderobject:");
+			UnityGraph.LogGameObject(renderObject);
 			renderObject.transform.parent = Prefab.transform;
 			renderObject.name = $"{Name}.Model";
 			renderObject.layer = Globals.inst.layerTank;
-
+			var collider = renderObject.EnsureComponent<BoxCollider>();
+			collider.size = Vector3.one;
 		}
 	}
 }
