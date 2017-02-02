5348c12bebb84b953bf984e95d7ec5615cdc2a18
mooshbeef <bramkamies@gmail.com>
2017-02-02 07:29:36 +0100

    Added new mod for custom cursor

---
+++ src/Nuterra/Maritaria/CustomCursor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Nuterra;
+
+namespace Maritaria
+{
+	[Mod]
+	public sealed class CustomCursor : TerraTechMod
+	{
+		public override string Name => "Custom Cursor";
+		public override string Description => "Allows the cursor to be changed using a png image";
+
+		public override void Load()
+		{
+			base.Load();
+			var objects = UnityEngine.SceneManagement.SceneManager.GetActiveScene().GetRootGameObjects();
+			var mousePointers = objects.SelectMany(o => o.GetComponentsInChildren<MousePointer>());
+			foreach (MousePointer pointer in mousePointers)
+			{
+				Console.WriteLine($"Pointer: {pointer} {pointer.GetHashCode()}");
+			}
+		}
+	}
+}
