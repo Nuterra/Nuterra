a73bad3ea332afdd767a2e5e2b3a85725e10c78d
maritaria <bramkamies@gmail.com>
2017-02-22 11:55:00 +0100

    Adding GodModeBlock

---
+++ src/Nuterra/Maritaria/CheatBlocks/GodModeBlock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Maritaria.CheatBlocks
+{
+	public sealed class GodModeBlock : CustomBlock
+	{
+		public static readonly int BlockID = 9003;
+
+		public int CustomBlock.BlockID => ID;
+		public string Name => "Godmode Block";
+		public string Description => "Protects all attached blocks from all incomming damage";
+		public BlockCategories Category => BlockCategories.Accessories;
+		public FactionSubTypes Faction => FactionSubTypes.EXP;
+		public Sprite DisplaySprite => null;
+
+		public GodModeBlock()
+		{
+		}
+
+	}
+}
---
+++ src/Nuterra/Maritaria/CheatBlocks/ModuleGodMode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Maritaria.CheatBlocks
+{
+	public sealed class ModuleGodMode : Module
+	{
+		private ComponentPool.PoolDelegates OnPool()
+		{
+			block.AttachEvent += OnAttach;
+			block.DetachEvent += OnDetach;
+			return new ComponentPool.PoolDelegates(OnSpawn, OnRecycle);
+		}
+
+		private void OnSpawn()
+		{
+		}
+
+		private void OnRecycle()
+		{
+		}
+
+		private void OnAttach()
+		{
+			foreach (TankBlock block in block.tank.blockman.IterateBlocks())
+			{
+				block.visible.damageable.SetInvulnerable(true, true);
+			}
+		}
+
+		private void OnDetach()
+		{
+			bool retainGodmode = false;
+			foreach (TankBlock block in block.tank.blockman.IterateBlocks())
+			{
+				var otherBlock = block.GetComponent<ModuleGodMode>();
+				if (otherBlock != this)
+				{
+					retainGodmode = true;
+					break;
+				}
+			}
+			if (!retainGodmode)
+			{
+				block.visible.damageable.SetInvulnerable(false, true);
+			}
+		}
+	}
+}
