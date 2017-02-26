8024ec1a19002592e92eb532bb24ac736fded401
maritaria <bramkamies@gmail.com>
2017-02-26 14:36:55 +0100

    Code cleanup

--- src/Nuterra/Maritaria/MagnetToggleKeyBehaviour.cs
+++ src/Nuterra/Maritaria/MagnetToggleKeyBehaviour.cs
@@ -38,14 +38,14 @@ namespace Maritaria
 			}
 			if (Modules.Magnet.DisabledForPlayerControlledTank)
 			{
-				_content.text=(string.Format(MagnetToggleKeyBehaviour.DisplayFormat, MagnetToggleKeyBehaviour.OfflineStatus));
+				_content.text = (string.Format(MagnetToggleKeyBehaviour.DisplayFormat, MagnetToggleKeyBehaviour.OfflineStatus));
 			}
 			else
 			{
-				_content.text=(string.Format(MagnetToggleKeyBehaviour.DisplayFormat, MagnetToggleKeyBehaviour.OnlineStatus));
+				_content.text = (string.Format(MagnetToggleKeyBehaviour.DisplayFormat, MagnetToggleKeyBehaviour.OnlineStatus));
 			}
-			_style.fontSize=(Screen.height / 60);
-			Vector2 vector=new Vector2((float)Screen.width * 0.5f, (float)Screen.height * 0.95f);
+			_style.fontSize = (Screen.height / 60);
+			Vector2 vector = new Vector2((float)Screen.width * 0.5f, (float)Screen.height * 0.95f);
 			Vector2 vector2 = _style.CalcSize(_content);
 			vector.y -= vector2.y * 0.5f;
 			vector.x -= vector2.x * 0.5f;
