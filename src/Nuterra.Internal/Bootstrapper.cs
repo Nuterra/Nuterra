0ef12a8ae56ce313ed6800b9b53014fae294fd0b
maritaria <bramkamies@gmail.com>
2017-03-01 00:36:08 +0100

    Added bootstrapper project

---
+++ src/Nuterra.Internal/Bootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace Nuterra.Internal
+{
+	internal sealed class Bootstrapper
+	{
+		public static void Startup()
+		{
+			Assembly nuterra = AppDomain.CurrentDomain.Load("Nuterra");
+			nuterra.GetType("Nuterra.ModLoader.Load()");
+		}
+	}
+}
---
+++ src/Nuterra.Internal/Hooks.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nuterra.Internal
+{
+	internal static class Hooks
+	{
+	}
+}
