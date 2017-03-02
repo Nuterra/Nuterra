5cccb6c8ec343217d68371bed27886623fc7f1ac
maritaria <bramkamies@gmail.com>
2017-03-02 17:58:55 +0100

    Implemented Hello World Mod

---
+++ src/Nuterra.HelloWorld/HelloWorldMod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nuterra.HelloWorld
+{
+	public sealed class HelloWorldMod : TerraTechMod
+	{
+		public override string Name => nameof(HelloWorldMod);
+		public override string Description => "Minimalistic mod that verifies your install of Nuterra works and mods load correctly";
+
+		public override void Load()
+		{
+			base.Load();
+			Console.WriteLine("Hello world");
+		}
+	}
+}
