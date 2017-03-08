e517e97f29257d82f121871444a79a538946118a
maritaria <bramkamies@gmail.com>
2017-03-08 02:10:38 +0100

    Fixed debug symbols crashing the game
    Unity crashes if you provide mono with debug symbols on a non debug
    player (checked using UnityEngine.Debug.isDebugBuild)

--- src/Nuterra.Internal/Bootstrapper.cs
+++ src/Nuterra.Internal/Bootstrapper.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using UnityEngine;
 
 namespace Nuterra.Internal
 {
@@ -10,7 +12,23 @@ namespace Nuterra.Internal
 	{
 		public static void Start()
 		{
-			Assembly nuterra = AppDomain.CurrentDomain.Load("Nuterra");
+			string managedDir = Path.Combine(Application.dataPath, "Managed");
+			string assemblyFile = Path.Combine(managedDir, "Nuterra.dll");
+			if (!File.Exists(assemblyFile))
+			{
+				Console.WriteLine($"Nuterra bootstrapper: Missing Nuterra assembly at {assemblyFile}");
+				return;
+			}
+			byte[] assemblyBytes = File.ReadAllBytes(assemblyFile);
+
+			string symbolFile = assemblyFile + ".mdb";
+			byte[] symbolBytes = null;
+			if (Debug.isDebugBuild && File.Exists(symbolFile))
+			{
+				symbolBytes = File.ReadAllBytes(symbolFile);
+			}
+			Assembly nuterra = Assembly.Load(assemblyBytes, symbolBytes);
+			Console.WriteLine("Nuterra assembly loaded");
 			Type nuterraMain = nuterra.GetType("Nuterra.NuterraApi");
 			MethodInfo start = nuterraMain.GetMethod("Start", BindingFlags.Static | BindingFlags.NonPublic);
 			start.Invoke(null, new object[] { });
