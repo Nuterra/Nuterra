3e30189f9690c5a8b4c0e2702b2627b368fcfc72
maritaria <bramkamies@gmail.com>
2017-04-12 17:25:40 +0200

    stashing changes here for a bit

--- src/Nuterra.Editor/CustomBlockPrefab.cs
+++ src/Nuterra.Editor/CustomBlockPrefab.cs
@@ -1,14 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace Nuterra.Editor
 {
+	[Serializable]
 	public class CustomBlockPrefab : MonoBehaviour
 	{
+		[Range(2000, 32000)]
 		public int BlockID;
 		public string Name;
+		[Multiline]
 		public string Description;
+		[Range(0, 100 * 1000)]
 		public int Price;
-		public Sprite DisplaySprite;
 		public Vector3 Dimensions;
+		public Sprite DisplaySprite;
+		public Vector3[] AttachmentPoints;
+		public MyCoolType CoolThing;
+	}
+	[Serializable]
+	public class MyCoolType : MonoBehaviour
+	{
+		public int TestA;
+		public int TestB;
 	}
 }
